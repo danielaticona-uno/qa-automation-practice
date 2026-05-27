d9d03d9d989c3ac1b587a4e50a000d239ff45b23
dxni <daniela.ticona@gmail.com>
2026-05-27 18:40:45 -0400

    Add checkout overview and complete page objects

---
+++ pages/CheckoutOverviewPage.ts
@@ -0,0 +1,88 @@
+import { Page, Locator } from '@playwright/test';
+
+export class CheckoutOverviewPage {
+
+    readonly page: Page;
+
+    readonly itemNames: Locator;
+    readonly itemPrices: Locator;
+
+    readonly subPrices: Locator;
+    readonly tax: Locator;
+    readonly totalPrice: Locator;
+
+    readonly paymentInfo: Locator;
+    readonly shippingInfo: Locator;
+
+    readonly cancelButton: Locator;
+    readonly finishButton: Locator;
+
+
+
+
+    constructor(page: Page) {
+        this.page = page;
+
+        this.itemNames = page.locator('.inventory_item_name');
+        this.itemPrices = page.locator('.inventory_item_price');
+
+        this.subPrices = page.locator('.summary_subtotal_label');
+        this.tax = page.locator('.summary_tax_label');
+        this.totalPrice = page.locator('[data-test="total-label"]');
+
+        this.paymentInfo = page.locator('[data-test="payment-info-value"]');
+        this.shippingInfo = page.locator('[data-test="shipping-info-value"]');
+
+
+        this.cancelButton = page.locator('[data-test="cancel"]');
+        this.finishButton = page.locator('[data-test="finish"]');
+
+    }
+
+    async finishCheckout(): Promise<void> {
+        await this.finishButton.click();
+    }
+
+    async cancelCheckout(): Promise<void> {
+        await this.cancelButton.click();
+    }
+
+    async getItemsNames(): Promise<string[]> {
+        return await this.itemNames.allTextContents();
+
+    }
+
+    async getItemPrices(): Promise<number[]> {
+        const pricesText = await this.itemPrices.allTextContents();
+        return pricesText.map(price => Number(price.replace('$', '')));
+
+    }
+
+    async getSubtotal(): Promise<number> {
+        const subTotalText = await this.subPrices.textContent();
+        return Number(subTotalText?.replace('Item total: $', '').trim());
+    }
+
+    async getTax(): Promise<number> {
+        const taxText = await this.tax.textContent();
+        return Number(taxText?.replace('Total: $', '').trim());
+    }
+
+    async getTotalPrice(): Promise<number> {
+        const totalText = await this.totalPrice.textContent();
+        return Number(totalText?.replace('Total: $', '').trim());
+    }
+
+     async getPaymentInfo(): Promise<string > {
+        const paymentText = await this.paymentInfo.textContent();
+        return paymentText?.trim() ||"";
+
+    }
+    
+    async getShippingInfo(): Promise<string > {
+        const shippingText = await this.shippingInfo.textContent();
+        return shippingText?.trim() ||"";
+
+    }
+
+}
